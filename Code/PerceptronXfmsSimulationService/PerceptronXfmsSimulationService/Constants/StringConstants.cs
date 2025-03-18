d2404a7860b2ee57b5d82a2cdd566eec0939d42e
Biomedical Informatics Research Laboratory <safee.ullah@gmail.com>
2025-03-18 13:02:33 +0500

    20250318

---
+++ Code/PerceptronXfmsSimulationService/PerceptronXfmsSimulationService/Constants/StringConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace PerceptronXfmsSimulationService.Constants
+{
+    public class StringConstants
+    {
+        public static string RootPath = @"F:\PERCEPTRONXFMS";   //D:\GitHub\02_WebTool\WebTool //"D:\FARHAN\00_LocalGitHub\WebTool";
+        public static string MatlabMainFileFullPath = RootPath + "\\ToolBox";   // Path will be updated based on dev or prod side folder structs
+        public static string MatlabScriptsFullPath = RootPath + "\\PerceptronXfmsSimulationService\\PerceptronXfmsSimulationService\\Utilities";
+        public static string ResultFolderPath = @"F:\ResultFolder";
+        public static string InputFolder = @"F:\Input";//"D:\PerceptronXfmsInputFolder";
+    }
+}
---
+++ Code/PerceptronXfmsSimulationService/PerceptronXfmsSimulationService/Utilities/Zipping.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.IO.Compression;
+using PerceptronXfmsSimulationService.Constants;
+
+namespace PerceptronXfmsSimulationService.Utilities
+{
+    public class Zipping
+    {
+        public string ZippingOutputFiles(string ResultFolderPath, string QueryResultFullPath, string Title, string QueryID)
+        {
+            string ZipFullFileName = ResultFolderPath + "\\" + Title + "_" + QueryID + ".zip";
+            if (File.Exists(ZipFullFileName))
+            {
+                File.Delete(ZipFullFileName); //Deleted Pre-existing file
+            }
+
+
+            string sa = Path.GetFileName(QueryResultFullPath);
+
+            ZipFile.CreateFromDirectory(QueryResultFullPath, ZipFullFileName);
+
+            return ZipFullFileName;
+        }
+    }
+}
